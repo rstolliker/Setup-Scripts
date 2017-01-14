c5dc698e934a2d8d14dab50d98c0abce6ff9f849
Ryan Stolliker <ryan.stolliker@gmail.com>
2017-01-13 20:10:08 -0800

    ubuntu add sbt

--- Ubuntu/programming-dep.sh
+++ Ubuntu/programming-dep.sh
@@ -23,3 +23,9 @@ sudo npm install -g typescript
 
 #lisp
 sudo apt-get install -y clisp
+
+#sbt (scala)
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
